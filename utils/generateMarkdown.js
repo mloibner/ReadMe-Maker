cf2515b3116e3a4a1880dacf8443a992631c9a4e
Monique Loibner <m.loibner@hotmail.com>
2020-08-17 22:06:49 +1000

    editing layout

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,55 +3,55 @@ function generateMarkdown(data) {
     ![NPM version](https://img.shields.io/badge/npm-6.14.4-green)
     ![GitHub license](https://img.shields.io/badge/License-${data.license}-blue.svg)
 
-    ## Description
+## Description
     
-    ${data.description}
+  ${data.description}
 
-    ## Table of Contents
-    - [Installation](#Installation)
-    - [Usage](#Usage)
-    - [Contributors](#Contributors)
-    - [Test](#Tests)
-    - [Licence](#Licence)
-    - [Project Repository](#Project Repository)
-    - [GitHub](#GitHub)
+## Table of Contents
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [Contributors](#Contributors)
+  - [Test](#Tests)
+  - [Licence](#Licence)
+  - [Project Repository](#Project Repository)
+  - [GitHub](#GitHub)
     
-    ## Installation
+## Installation
 
-    To install necessary dependencies, run the following command:
+  To install necessary dependencies, run the following command:
     ~~~
     ${data.installation_instructions}
     ~~~
           
 
-    ## Usage 
+## Usage 
     
-    ${data.usage_information}
+  ${data.usage_information}
 
-    ### Contributors
-    
-    ${data.contribution_guidelines}
-    
-    ### Test
+### Contributors
+   
+  ${data.contribution_guidelines}
+   
+### Test
 
-    To run tests, run the following command:
+  To run tests, run the following command:
     ~~~
     ${data.test_instructions}
     ~~~
     
-    ### Licence
+### Licence
     
-    ${data.license}
+  ${data.license}
     
-    ### Repository
+### Repository
     
-    - [Project Repository](${data.github})
+  - [Project Repository](${data.github})
+  
+### GitHub
+    
+  - [GitHub](${data.email})
 
-    ### GitHub
-    
-    - [GitHub](${data.email})
-
-  `;
-  }
+ `;
+}
   
   module.exports = generateMarkdown;
